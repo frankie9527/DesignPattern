7f17f6abc667d2163c2da6a7f306c1825430f2c5
Frankie <847145851@qq.com>
2024-01-24 18:29:04 +0800

    add BuilderPattern

---
+++ app/src/main/java/com/design/pattern/builder/BuilderPattern.java
@@ -0,0 +1,14 @@
+package com.design.pattern.builder;
+
+
+import com.design.pattern.PrintLog;
+
+public class BuilderPattern {
+    public static void main(String[] args) {
+        PersonalInfo person = new PersonalInfo.Builder().
+                name("frankie").personAge(33).
+                phoneNum("+8613668232308").inCome("¥20000").build();
+        PrintLog.getInstance().show(person.toString());
+
+    }
+}
---
+++ app/src/main/java/com/design/pattern/builder/PersonalInfo.java
@@ -0,0 +1,63 @@
+package com.design.pattern.builder;
+
+import android.support.annotation.NonNull;
+
+public class PersonalInfo {
+    private final String name;
+    private final int age;
+    private final String phoneNum;
+    private final String inCome;
+
+    PersonalInfo(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+        this.phoneNum = builder.phoneNum;
+        this.inCome = builder.inCome;
+    }
+
+    public static final class Builder {
+        private String name;
+        private int age;
+        private String phoneNum;
+        private String inCome;
+
+        public Builder() {
+
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder personAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder phoneNum(String num) {
+            this.phoneNum = num;
+            return this;
+        }
+
+        public Builder inCome(String inCome) {
+            this.inCome = inCome;
+            return this;
+        }
+
+        public PersonalInfo build() {
+            return new PersonalInfo(this);
+        }
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "PersonalInfo{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", inCome='" + inCome + '\'' +
+                '}';
+    }
+}
