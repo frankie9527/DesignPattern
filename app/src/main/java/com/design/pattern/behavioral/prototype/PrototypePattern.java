ef337666645cbfe4be300aa3264c8d8dc161c3a3
frankie9527 <847145851@qq.com>
2024-07-01 00:25:32 +0800

    原型模式

---
+++ app/src/main/java/com/design/pattern/behavioral/prototype/PrototypePattern.java
@@ -0,0 +1,27 @@
+package com.design.pattern.behavioral.prototype;
+
+/**
+ * author: Frankie
+ * Date: 2024/7/1
+ * Description:
+ * 快速生成与原型对象相同的实例
+ */
+public class PrototypePattern {
+    public static void main(String[] args){
+        WordDocument originDoc = new WordDocument();
+        originDoc.setText("这是一篇文档");
+        originDoc.addImage("图片1");
+        originDoc.addImage("图片2");
+        originDoc.addImage("图片3");
+        originDoc.showDocument();
+
+        WordDocument doc2 = originDoc.clone();
+        doc2.showDocument();
+
+        doc2.setText("这是修改过的Doc2文本");
+        doc2.addImage("哈哈.jpg");
+        doc2.showDocument();
+
+        originDoc.showDocument();
+    }
+}
---
+++ app/src/main/java/com/design/pattern/behavioral/prototype/WordDocument.java
@@ -0,0 +1,73 @@
+package com.design.pattern.behavioral.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * author: Frankie
+ * Date: 2024/7/1
+ * Description:
+ */
+public class WordDocument implements Cloneable{
+    /**
+     * 文本
+     */
+    private String mText;
+    /**
+     * 图片名列表
+     */
+    private ArrayList<String>  mImages = new ArrayList<String>();
+
+    public WordDocument() {
+        System.out.println("----------- WordDocument构造函数 -----------");
+    }
+
+    /**
+     * 克隆对象
+     */
+    @Override
+    protected WordDocument clone() {
+        try {
+            WordDocument doc = (WordDocument) super.clone();
+            doc.mText = this.mText;
+//            doc.mImages = this.mImages;
+            doc.mImages = (ArrayList<String>) this.mImages.clone();
+            return doc;
+        } catch (Exception e) {
+        }
+
+        return null;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String mText) {
+        this.mText = mText;
+    }
+
+    public List<String> getImages() {
+        return mImages;
+    }
+
+    /**
+     * @param img
+     */
+    public void addImage(String img) {
+        this.mImages.add(img);
+    }
+
+    /**
+     * 打印文档内容
+     */
+    public void showDocument() {
+        System.out.println("----------- Word Content Start -----------");
+        System.out.println("Text : " + mText);
+        System.out.println("Images List: ");
+        for (String imgName : mImages) {
+            System.out.println("image name : " + imgName);
+        }
+        System.out.println("----------- Word Content End -----------");
+    }
+}
